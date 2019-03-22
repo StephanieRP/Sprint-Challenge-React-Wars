1c92520f74202ad9531fda2aeb5bf8191263f5e8
Stephanie Patton <31425878+StephanieRP@users.noreply.github.com>
2019-03-22 15:52:25 -0500

    add stretch

---
+++ dogs/src/components/doglist.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../style.css";
+
+const DogList = props => {
+  // console.log("Doglist:", props);
+  return <img className="cute-dogs" src={props.dog} alt="cute dogs" />;
+};
+
+export default DogList;
